387ac3381adf93843bf519cdaf7c5fded05bd39a
jh3y <jh3y@users.noreply.github.com>
2020-10-13 22:19:09 +0100

    feat: tidy dependencies and bring in emotion for CSS in JS solution

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import styled from '@emotion/styled'
 import { ipcRenderer } from 'electron'
+
+const MESSAGE = 'Howdy'
+
+const Box = styled.div`
+  height: 100px;
+  width: 100px;
+  background: hsl(200, 80%, 50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`
 
 const App = () => {
   const sendMessage = () => {
-    ipcRenderer.send('message-send', 'Hey!')
+    ipcRenderer.send('message-send', MESSAGE)
   }
 
   return (
-    <div className="app">
-      <button onClick={sendMessage}>Say "Hey" from the other side! 👋</button>
-    </div>
+    <Fragment>
+      <h1>Parcel React Electron Starter</h1>
+      <Box>A Blue Box styled with emotion</Box>
+      <button
+        onClick={
+          sendMessage
+        }>{`Say "${MESSAGE}" from the other side! 👋`}</button>
+    </Fragment>
   )
 }
 
